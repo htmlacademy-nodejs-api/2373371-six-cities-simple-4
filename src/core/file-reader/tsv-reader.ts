a260185d32fa17f1bc2ffd4a64b2ce59684b5a02
Evgenii Khristenko <evgenii_khristenko1@epam.com>
2023-05-12 14:46:11 +0400

    change read chunk size

--- src/core/file-reader/tsv-reader.ts
+++ src/core/file-reader/tsv-reader.ts
@@ -2,7 +2,7 @@ import { FileReaderInterface } from './file-reader.interface.js';
 import EventEmitter from 'node:events';
 import { createReadStream } from 'node:fs';
 
-const CHUNK_SIZE = 16000;
+const CHUNK_SIZE = 16384; // 16KB
 
 export default class TSVReader extends EventEmitter implements FileReaderInterface {
 
@@ -11,7 +11,10 @@ export default class TSVReader extends EventEmitter implements FileReaderInterfa
   }
 
   public async read(): Promise<void> {
-    const readStream = createReadStream(this.filename, { encoding: 'utf-8', highWaterMark: CHUNK_SIZE });
+    const readStream = createReadStream(this.filename, {
+      encoding: 'utf-8',
+      highWaterMark: CHUNK_SIZE
+    });
 
     let remainingData = '';
     let positionOfSeparator = remainingData.indexOf('\n');
