ea48657356d4513f5760591410b0b3b9d6777909
Evgenii Khristenko <evgenii_khristenko1@epam.com>
2023-05-12 14:43:03 +0400

    add cities mapper

---
+++ src/constants/city.ts
@@ -0,0 +1,10 @@
+import { Cities } from '../types/city.type';
+
+export const CITY_TO_COORDINATES: Record<Cities, [string, string]> = {
+  Amsterdam: ['52.370216', '4.895168'],
+  Paris: ['48.85661', '2.351499'],
+  Cologne: ['50.938361', '6.959974'],
+  Brussels: ['50.846557', '4.351697'],
+  Hamburg: ['53.550341', '10.000654'],
+  Dusseldorf: ['51.225402', '6.776314'],
+};
